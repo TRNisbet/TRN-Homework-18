0d8f7e6f83668f320a1314a2b6e8a066c712b261
TRNisbet <TRNisbet@hotmail.com>
2021-02-01 18:32:05 -0500

    second attempt

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
+require('dotenv').config()
 const PORT = 3000;
 
 const app = express();
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
